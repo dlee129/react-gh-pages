69b034273ad82f2f0929ae15776fb30bdd95719a
dlee129 <david.lee09@qmail.cuny.edu>
2018-10-03 21:36:29 -0400

    Updates

---
+++ precache-manifest.3c7dff0c0b6ceb509e2e51940830c1ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "559a0da3d49c489013bd",
+    "url": "/react-gh-pages/static/js/main.559a0da3.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/react-gh-pages/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "559a0da3d49c489013bd",
+    "url": "/react-gh-pages/static/css/main.8dbd45ce.chunk.css"
+  },
+  {
+    "revision": "1e185b0435abd6cd557c328986abc2a4",
+    "url": "/react-gh-pages/index.html"
+  }
+];
